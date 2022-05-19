0c1abf9c149a9135b630b147214e0ec339904f46
svoh86 <sms-86@mail.ru>
2022-05-19 12:20:09 +0300

    Архитектура проекта - Аргегатор Java Вакансий [#260359]

--- src/main/java/grabber/Grab.java
+++ src/main/java/grabber/Grab.java
@@ -3,6 +3,10 @@ package grabber;
 import org.quartz.Scheduler;
 import org.quartz.SchedulerException;
 
+/**
+ * Будем использовать quartz для запуска парсера. Но напрямую мы не будем его использовать.
+ * Абстрагируемся через интерфейс.
+ */
 public interface Grab {
     void init(Parse parse, Store store, Scheduler scheduler) throws SchedulerException;
 }
--- src/main/java/grabber/Store.java
+++ src/main/java/grabber/Store.java
@@ -2,6 +2,10 @@ package grabber;
 
 import java.util.List;
 
+/**
+ * Описание компонента через интерфейс позволяет расширить наш проект:
+ * осуществить сбор данных с других площадок
+ */
 public interface Store {
     void save(Post post);
 
